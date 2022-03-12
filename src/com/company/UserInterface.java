2b3f5ac96fe7428a3b32098d9db22a634b93d8ee
SebastianHeiberg <seba3673@stud.kea.dk>
2022-03-12 18:14:08 +0100

    finished color to Level text

--- src/com/company/UserInterface.java
+++ src/com/company/UserInterface.java
@@ -6,26 +6,32 @@ public class UserInterface {
   public static final String ANSI_GREEN = "\u001B[32m";
   public static final String ANSI_YELLOW = "\u001B[33m";
   public static final String ANSI_RESET = "\u001B[0m";
-  public static final String ANSI_CYAN = "\u001B[36m";
 
-
-  public void displayRiskArray (Risk [] riskArray) {
+  public void displayRiskArray(Risk[] riskArray) {
     System.out.println("Nr.  Risk name \t\t pro  con\tsev\t Level");
     for (int i = 0; i < riskArray.length; i++) {
-      System.out.printf("# %2d %3s\t %d \t  %d \t%d", i,riskArray[i].getName(),riskArray[i].getProbability(),
-          riskArray[i].getConsequense(),riskArray[i].getSeverity());
 
-      if (riskArray[i].getSeverity()<=5) {
-        System.out.print("\t Small");
-      } else if (riskArray[i].getSeverity() <= 15 ) {
-        System.out.print("\t Not that small");
-      }
-      if (riskArray[i].getSeverity()>15) {
-        System.out.print("\t something big");
-      }
+      System.out.printf("# %2d %3s\t %d \t  %d \t%d", i, riskArray[i].getName(), riskArray[i].getProbability(),
+          riskArray[i].getConsequense(), riskArray[i].getSeverity());
+
+      displaySeverityLevel(riskArray[i].getSeverity());
       System.out.println();
     }
   }
 
+  public void displaySeverityLevel(int degreeOfSeverity) {
+    if (degreeOfSeverity <= 5) {
+      System.out.print("\t" + ANSI_GREEN + " Very_Low" + ANSI_RESET);
+    } else if (degreeOfSeverity >= 10 && degreeOfSeverity < 15) {
+      System.out.print("\t" + ANSI_GREEN + " Low" + ANSI_RESET);
+    } else if (degreeOfSeverity > 10 && degreeOfSeverity <= 15) {
+      System.out.print("\t" + ANSI_YELLOW + " Medium" + ANSI_RESET);
+    } else if (degreeOfSeverity > 15 && degreeOfSeverity <= 20) {
+      System.out.print("\t" + ANSI_RED + " High" + ANSI_RESET);
+    } else {
+      System.out.print("\t" + ANSI_RED + " Very_High" + ANSI_RESET);
+    }
+
+  }
 
 }
